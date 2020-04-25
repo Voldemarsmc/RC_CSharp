4c36f301daa38bdb86584075c11b380ac35057e1
voldemars <ozols.voldemars123@gmail.com>
2020-04-25 12:38:18 +0300

    r

---
+++ day3_loops/day3_loops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace day3_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForExample();
+        }
+        static void ForExample()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);            }
+        }
+    }
+}
