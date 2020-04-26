e9af87d7da210c9f1acdae25520b88756ed93017
voldemars <ozols.voldemars123@gmail.com>
2020-04-26 20:31:57 +0300

    majasdarbs day3

---
+++ day3MD/day3MD/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace day3MD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            zvaigznites2();
+        }
+
+
+        static void zvaigznites2()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+
+
+            for (int i = 4; i >= 1; i--)
+            {
+                for (int k = 1; k < i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
--- day3_loops/day3_loops/Program.cs
+++ day3_loops/day3_loops/Program.cs
@@ -6,13 +6,41 @@ namespace day3_loops
     {
         static void Main(string[] args)
         {
-            ForExample();
+            uzdDay3();
         }
-        static void ForExample()
+        static void uzdDay3()
         {
-            for(int i = 0; i < 10; i++)
+
+
+
+   int i, j, n;
+   
+
+            n = 7;   
+   for(i = 0; i < n; i++)
+   {
+
+     for(j = 1 ; j <= 2 * i - 1; j++)
+  
+                
+                for (j = 1 ; j <= 2 * i + 1; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+   }
+            for (i = 10; i < n; i++)
             {
-                Console.WriteLine(i);            }
-        }
+
+
+
+                for (j = 3; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+
+            }
+
+
+
+
+            }
     }
 }
