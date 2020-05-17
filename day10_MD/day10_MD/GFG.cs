208cc44d33d98818eb558bc6cc8fee7332e91bc3
voldemars <ozols.voldemars123@gmail.com>
2020-05-17 15:59:24 +0300

    r

--- day10_MD/day10_MD/GFG.cs
+++ day10_MD/day10_MD/GFG.cs
@@ -6,5 +6,62 @@ namespace day10_MD
 {
     class GFG
     {
+       
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+        public static void compAndSwap(int[] a, int i, int j, int dir)
+        {
+
+            int k;
+            if ((a[i] > a[j]))
+                k = 1;
+            else
+                k = 0;
+            if (dir == k)
+                Swap(ref a[i], ref a[j]);
+        }
+
+
+        public static void bitonicMerge(int[] a, int low, int cnt, int dir)
+        {
+            if (cnt > 1)
+            {
+                int k = cnt / 2;
+                for (int i = low; i < low + k; i++)
+                    compAndSwap(a, i, i + k, dir);
+                bitonicMerge(a, low, k, dir);
+                bitonicMerge(a, low + k, k, dir);
+            }
+        }
+
+
+        public static void bitonicSort(int[] a, int low, int cnt, int dir)
+        {
+            if (cnt > 1)
+            {
+                int k = cnt / 2;
+
+                 
+                bitonicSort(a, low, k, 1);
+
+                 
+                bitonicSort(a, low + k, k, 0);
+
+               
+                bitonicMerge(a, low, cnt, dir);
+            }
+        }
+
+
+        public static void sort(int[] a, int N, int up)
+        {
+            bitonicSort(a, 0, N, up);
+        }
+
     }
 }
--- day10_MD/day10_MD/Program.cs
+++ day10_MD/day10_MD/Program.cs
@@ -6,7 +6,57 @@ namespace day10_MD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            CycleSort();
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine(elapsedMs);
+
+            var watch2 = System.Diagnostics.Stopwatch.StartNew();
+            bitcoin();
+            watch.Stop();
+            var elapsedMs2 = watch.ElapsedMilliseconds;
+            Console.WriteLine(elapsedMs2);
+
+            Console.WriteLine();
+            Console.WriteLine(DateTime.Now.ToString("h:mm:ss tt"));
+
         }
+
+
+
+
+
+        static void CycleSort()
+        {
+            int[] arr = new int[10000];
+            int n = arr.Length;
+
+            
+            GFG2.cycleSort(arr, n);
+
+            Console.Write("After sort : ");
+            for (int i = 0; i < n; i++) ;
+                
+        }
+
+
+
+
+        static void bitcoin()
+        {
+            int[] a = new int[10000];
+            int N = a.Length;
+
+            int up = 1;   
+            GFG.sort(a, N, up);
+            Console.WriteLine();
+            Console.Write("Sorted array: ");
+            for (int i = 0; i < N; i++) ;
+               
+        }
+    
+    
     }
 }
