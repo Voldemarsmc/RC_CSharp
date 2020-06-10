f7f1853ef0ba15208d1965114be21b827e376cce
voldemars <ozols.voldemars123@gmail.com>
2020-06-10 18:02:54 +0300

    r

--- day19_SQL/sql.sql
+++ day19_SQL/sql.sql
@@ -9,16 +9,46 @@ employeeLastName varchar(20)
 
 drop table emplyees;
 
+Alter table employees
+modify column employeeId int primary key AUTO_INCREMENT; 
+
 insert into employees  (employeeId, employeeName, employeeLastname)
 values(1, "juris", "berzins"),
 (2, "anete", "cauna");
-
-Alter table employees
-modify column employeeId int primary key
 
 
 insert into employees(employeeName, employeeLastName)
 values("karlis", "marks");
 
+select employeeName, employeeLastName
 from employees
 where employeeId = 1;
+
+
+create table studenti
+(
+studentId int NOT NULL,
+studentCourse int NOT NULL,
+studentName varchar(20) NOT NULL,
+studentLastName varchar(20) NOT NULL 
+);
+
+Alter table studenti
+modify column studentId int primary key AUTO_INCREMENT; 
+
+ insert into  studenti(studentId, studentCourse, studentName, studentLastName)
+ values (1, 1, "janis", "kalnins"),
+ (2, 1, "juris", "berzins"),
+ (3, 1, "andris", "liepins"),
+ (4, 1, "janis", "ozolins"),
+ (5, 2, "davis", "lapins"),
+ (6, 2, "arturs", "zarins"),
+ (7, 2, "artis", "ozols"),
+ (8, 3, "jurgis", "petersislis"),
+ (9, 3, "lauris", "alksnitis"),
+ (10, 3, "maija", "liepa"),
+ (11, 3, "liene", "ziediņa"),
+ (12, 3, "markus", "rozits");
+
+select * from studenti
+where studentCourse = 2;
